4ab86565faa8af6d6f1bb6f662c7d87d2464af48
kostya05983 <kostya05983@mail.ru>
2019-04-26 13:38:35 +0700

    [UPD] end with huffman decoding

--- src/main/kotlin/stepik/HuffmanDecoding.kt
+++ src/main/kotlin/stepik/HuffmanDecoding.kt
@@ -2,25 +2,37 @@ package stepik
 
 import java.util.*
 
-data class HuffmanNode(
-    var left: Node? = null,
-    var right: Node? = null,
-    val char: Char,
-    val code: String
-)
+private fun decode(codes: Map<String, Char>, line: String): String {
+    var currentLine = line
+    val sb = StringBuilder()
+    while (currentLine.isNotEmpty()) {
+        for ((key, value) in codes) {
+            if (currentLine.startsWith(key)) {
+                currentLine = currentLine.substring(key.length)
+                sb.append(value)
+            }
+        }
+    }
+    return sb.toString()
+}
 
-//private fun from
 
 fun main() {
     val scanner = Scanner(System.`in`)
     val firstLine = scanner.nextLine().split(" ")
 
     val amountChars = firstLine[0].toInt()
-    val lineLength = firstLine[1].toInt()
+
+    val map = mutableMapOf<String, Char>()
 
     for (i in 0 until amountChars) {
         val line = scanner.nextLine().split(" ")
         val char = line[0].replace(":", "")[0]
-        val node = HuffmanNode(null, null, char, line[2])
+        map[line[1]] = char
     }
+
+    val encodeStr = scanner.nextLine()
+
+    val decode = decode(map, encodeStr)
+    println(decode)
 }
