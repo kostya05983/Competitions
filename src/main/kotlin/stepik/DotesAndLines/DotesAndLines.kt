8b82719d2aa354a32a1c28cd83b0d0ad76a444e4
kostya05983 <kostya05983@mail.ru>
2019-05-02 21:48:45 +0700

    [UPD] solve the task with search

--- src/main/kotlin/stepik/DotesAndLines/DotesAndLines.kt
+++ src/main/kotlin/stepik/DotesAndLines/DotesAndLines.kt
@@ -2,65 +2,24 @@ package stepik.DotesAndLines
 
 import java.util.*
 
-private data class Line(val start: Int, val end: Int)
 
-private fun contains(dot: Int, line: Line): Boolean {
-    return dot >= line.start && dot <= line.end
-}
-
-private class SearchAlgorithm {
-    fun countAmountLinesForDotesBinary(lines: Array<Line>, left: Int, right: Int, dot: Int): Int {
-        val middle = (right - left) / 2 + left
-        var amount = 0
-
-        if (right >= left) {
-            if (contains(dot, lines[middle])) {
-                for (i in left..right) {
-                    if (contains(dot, lines[i]))
-                        amount++
-                }
-                return amount
-            }
-
-            amount += if (lines[middle].end > dot) {
-                countAmountLinesForDotesBinary(lines, left, middle - 1, dot)
-            } else {
-                countAmountLinesForDotesBinary(lines, middle + 1, right, dot)
-            }
+private fun iterativeAnotherType(lines: Array<NewLine>, amountDotes: Int): IntArray {
+    var amount = 0
+    val intArray = IntArray(amountDotes)
+    for (i in 0 until lines.size) {
+        when {
+            lines[i].type == -1 -> amount++
+            lines[i].type == +1 -> amount--
+            else -> intArray[lines[i].number] = amount
         }
-
-        return amount
     }
 
-    fun iterativeBinarySearch(lines: Array<Line>, left: Int, right: Int, dot: Int): Int {
-        var middle = (right - left) / 2 + left
-        var amount = 0
-
-        var currentLeft = left
-        var currentRight = right
-
-        while (!contains(dot, lines[middle]) && currentRight >= currentLeft) {
-            middle = (currentRight - currentLeft) / 2 + currentLeft
-            if (lines[middle].end > dot) {
-                currentRight = middle - 1
-            } else {
-                currentLeft = middle + 1
-            }
-        }
-
-        if (contains(dot, lines[middle])) {
-            for (i in left..right) {
-                if (contains(dot, lines[i]))
-                    amount++
-            }
-        }
-        return amount
-    }
+    return intArray
 }
 
+private class NewLine(val dote: Int, val type: Int, val number: Int)
 
 fun main() {
-    val startTime = System.currentTimeMillis()
     val scanner = Scanner(System.`in`)
 
     val firstLine = scanner.nextLine().split(" ")
@@ -68,48 +27,45 @@ fun main() {
     val amountLines = firstLine[0].toInt()
     val amountDotes = firstLine[1].toInt()
 
-    val lines = Array(amountLines) { Line(0, 0) }
+    val lines = Array(amountLines * 2 + amountDotes) { NewLine(0, -2, -1) }
 
-    for (i in 0 until amountLines) {
+    for (i in 0 until amountLines * 2 step 2) {
         val line = scanner.nextLine()
         val index = line.indexOf(" ")
+
         val first = line.substring(0, line.indexOf(" "))
         val second = line.substring(index + 1)
-        lines[i] = Line(first.toInt(), second.toInt())
+        lines[i] = NewLine(first.toInt(), -1, -1)
+        lines[i + 1] = NewLine(second.toInt(), +1, -1)
     }
 
     val dotesStr = scanner.nextLine()
 
-    val dotes = IntArray(amountDotes)
-
     var index = -1
-    for (i in 0 until amountDotes - 1) {
+    val start = amountLines * 2
+
+    for ((position, i) in (start until start + amountDotes - 1).withIndex()) {
         val old = index + 1
         index = dotesStr.indexOf(' ', old)
-        dotes[i] = dotesStr.substring(old, index).toInt()
-    }
-    dotes[amountDotes - 1] = dotesStr.substring(index + 1).toInt()
-
-    lines.sortBy {
-        it.end
+        lines[i] = NewLine(dotesStr.substring(old, index).toInt(), 0, position)
     }
 
-    val searchAlgorithm = SearchAlgorithm()
+    lines[start + amountDotes - 1] = NewLine(dotesStr.substring(index + 1).toInt(), 0, amountDotes - 1)
+
+    lines.sortWith(Comparator { o1: NewLine, o2: NewLine ->
+        if (o1.dote != o2.dote) {
+            return@Comparator o1.dote.compareTo(o2.dote)
+        } else {
+            return@Comparator o1.type.compareTo(o2.type)
+        }
+    })
+
 
     val sb = StringBuilder()
-    for (dote in dotes) {
-        sb.append(searchAlgorithm.iterativeBinarySearch(lines, 0, lines.size - 1, dote)).append(" ")
+    val result = iterativeAnotherType(lines, amountDotes)
+    for (answer in result) {
+        sb.append(answer).append(' ')
     }
-    print(sb.toString())
 
-    println(System.currentTimeMillis() - startTime)
+    print(sb.toString())
 }
-//
-//6 6
-//0 3
-//1 3
-//2 3
-//3 4
-//3 5
-//3 6
-//1 2 3 4 5 6
