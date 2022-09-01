e39967df6bcb65bc632cb5d8476acd09d46b4a5b
kostya05983 <kostya05983@mail.ru>
2022-09-02 00:10:00 +0700

    Add remove duplicates solution

---
+++ src/main/kotlin/leetCode/RemoveDuplicates.kt
@@ -0,0 +1,34 @@
+package leetCode
+
+class RemoveDuplicates {
+    fun removeDuplicates(nums: IntArray): Int {
+        var currentSize = nums.size
+        var i = 0
+        while (i < currentSize - 1) {
+            if (nums[i] == nums[i + 1]) {
+                currentSize--
+                leftShiftFrom(i, nums)
+            }
+            if (nums[i] != nums[i + 1]) {
+                i++
+            }
+        }
+        return currentSize
+    }
+
+    private fun leftShiftFrom(position: Int, nums: IntArray) {
+        var current = position
+        while (current < nums.size - 1) {
+            nums[current] = nums[current + 1]
+            current++
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    val removeDuplicates = RemoveDuplicates()
+    val result = intArrayOf(0, 0, 1, 1, 1, 2, 2, 3, 3, 4)
+    val size = removeDuplicates.removeDuplicates(result)
+    println(size)
+    println(result.toList())
+}
