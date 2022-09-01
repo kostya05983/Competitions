11ee8f022289e07032ffd436788f0cbe5d1c9b74
kostya05983 <kostya05983@mail.ru>
2022-09-02 01:42:32 +0700

    Add valid mountain array

---
+++ src/main/kotlin/leetCode/ValidMountainArray.kt
@@ -0,0 +1,24 @@
+package leetCode
+
+class ValidMountainArray {
+    fun validMountainArray(arr: IntArray): Boolean {
+        var peek = false
+        var up = false
+        var down = false
+        for (i in 0 until arr.size - 1) {
+            if (arr[i] < arr[i + 1] && !peek) {
+                up = true
+            } else if (arr[i] > arr[i + 1] && up) {
+                peek = true
+                down = true
+            } else {
+                return false
+            }
+        }
+        return up && down
+    }
+}
+
+fun main(args: Array<String>) {
+    println(ValidMountainArray().validMountainArray(intArrayOf(0,3,2,1)))
+}
