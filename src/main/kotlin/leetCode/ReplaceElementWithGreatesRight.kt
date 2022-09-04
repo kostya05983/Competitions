c6ea631e8eb30072e84be2dd9e3b77e74ff82698
kostya05983 <kostya05983@mail.ru>
2022-09-04 16:06:58 +0700

    Add replaceElementWithGreatesRight

---
+++ src/main/kotlin/leetCode/ReplaceElementWithGreatesRight.kt
@@ -0,0 +1,26 @@
+package leetCode
+
+class ReplaceElementWithGreatesRight {
+    fun replaceElements(arr: IntArray): IntArray {
+        for (i in 0 until arr.size) {
+            arr[i] = findGreatestElement(i + 1, arr)
+        }
+        return arr
+    }
+
+    private fun findGreatestElement(index: Int, arr: IntArray): Int {
+        if (index == arr.size) return -1
+
+        var max = -1
+        for (i in index until arr.size) {
+            if (arr[i] > max) {
+                max = arr[i]
+            }
+        }
+        return max
+    }
+}
+
+fun main(args: Array<String>) {
+    println(ReplaceElementWithGreatesRight().replaceElements(intArrayOf(400)).toList())
+}
