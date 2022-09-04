fce9f8b6389de152939e8220803e0bcf1b98a217
kostya05983 <kostya05983@mail.ru>
2022-09-04 16:18:50 +0700

    RemoveDuplicatesInPlace

---
+++ src/main/kotlin/leetCode/RemoveDuplicatesInPlace.kt
@@ -0,0 +1,20 @@
+package leetCode
+
+class RemoveDuplicatesInPlace {
+    fun removeDuplicates(nums: IntArray): Int {
+        var uniqueElements = 1
+        for (i in 0 until nums.size - 1) {
+            if (nums[i] != nums[i + 1]) {
+                nums[uniqueElements] = nums[i + 1]
+                uniqueElements++
+            }
+        }
+        return uniqueElements
+    }
+}
+
+fun main(args: Array<String>) {
+    val result = intArrayOf(1,2)
+    println(RemoveDuplicatesInPlace().removeDuplicates(result))
+    println(result.toList())
+}
