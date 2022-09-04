ec6a79b243f7bc82cec52bc6f970b6f9c801af2c
kostya05983 <kostya05983@mail.ru>
2022-09-04 22:59:59 +0700

    Add height checker

---
+++ src/main/kotlin/leetCode/HeightChecker.kt
@@ -0,0 +1,18 @@
+package leetCode
+
+class HeightChecker {
+    fun heightChecker(heights: IntArray): Int {
+        val sorted = heights.sorted()
+        var counter = 0
+        for (i in heights.indices) {
+            if (heights[i] != sorted[i]) {
+                counter++
+            }
+        }
+        return counter
+    }
+}
+
+fun main(args: Array<String>) {
+    println(HeightChecker().heightChecker(intArrayOf(1,1,4,2,1,3)))
+}
