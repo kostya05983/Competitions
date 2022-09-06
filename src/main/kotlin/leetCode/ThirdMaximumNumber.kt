eef348b7388ba5d03dac7c46951f27e22e0398e4
kostya05983 <kostya05983@mail.ru>
2022-09-06 23:43:10 +0700

    Add third maximum number

---
+++ src/main/kotlin/leetCode/ThirdMaximumNumber.kt
@@ -0,0 +1,33 @@
+package leetCode
+
+class ThirdMaximumNumber {
+    fun thirdMax(nums: IntArray): Int {
+        var firstMax: Int? = null
+        var secondMax: Int? = null
+        var thirdMax: Int? = null
+        for (i in nums.indices) {
+            if (nums[i] == thirdMax || nums[i] == secondMax || nums[i] == firstMax) {
+                continue
+            }
+
+            if (firstMax == null || firstMax < nums[i]) {
+                thirdMax = secondMax
+                secondMax = firstMax
+                firstMax = nums[i]
+            } else if (secondMax == null || secondMax < nums[i]) {
+                thirdMax = secondMax
+                secondMax = nums[i]
+            } else if (thirdMax == null || thirdMax < nums[i]) {
+                thirdMax = nums[i]
+            }
+        }
+        return if (thirdMax == null && firstMax != null) {
+            firstMax
+        } else thirdMax ?: throw RuntimeException("Unsupported case")
+    }
+}
+
+fun main(args: Array<String>) {
+    val result = intArrayOf(3, 2, 1)
+    println(ThirdMaximumNumber().thirdMax(result))
+}
