aa5ed8e2643027259a467f7a6b170ec437c378d4
kostya05983 <kostya05983@mail.ru>
2022-10-04 22:55:54 +0700

    Fizzbuzz

---
+++ src/main/kotlin/leetCode/FizzBuzzSolution.kt
@@ -0,0 +1,28 @@
+package leetCode
+
+class FizzBuzzSolution {
+    fun fizzBuzz(n: Int): List<String> {
+        var i = 1
+        val result = ArrayList<String>(n)
+        while (i <= n) {
+            when {
+                i.rem(3) == 0 && i.rem(5) == 0 -> {
+                    result.add("FizzBuzz")
+                }
+                i.rem(3) == 0 -> {
+                    result.add("Fizz")
+                }
+                i.rem(5) == 0 -> {
+                    result.add("Buzz")
+                }
+                else -> result.add(i.toString())
+            }
+            i++
+        }
+        return result
+    }
+}
+
+fun main(args: Array<String>) {
+    println(FizzBuzzSolution().fizzBuzz(15))
+}
