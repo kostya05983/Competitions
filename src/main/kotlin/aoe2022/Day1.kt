f48a3bb6bb480583f1f70c5c4b33a3ab5cc08b02
kostya05983 <kostya05983@mail.ru>
2023-01-04 02:41:40 +0700

    Second start

--- src/main/kotlin/aoe2022/Day1.kt
+++ src/main/kotlin/aoe2022/Day1.kt
@@ -1,6 +1,7 @@
 package aoe2022
 
 import java.io.File
+import java.util.*
 import kotlin.math.max
 
 class Day1 {
@@ -22,6 +23,24 @@ class Day1 {
         }
 
         return sum
+    }
+
+    fun solve2(): Int {
+        val input = File("/home/kostya05983/Projects/Competitions/src/main/resources/input.txt").readLines()
+
+        val priorityQueueHeap = PriorityQueue(Collections.reverseOrder<Int>())
+        var currentSum = 0
+        for (i in input.indices) {
+            val number = input[i].toIntOrNull()
+            if (number == null) {
+                priorityQueueHeap.add(currentSum)
+                currentSum = 0
+            } else {
+                currentSum += number
+            }
+        }
+
+        return priorityQueueHeap.poll()+priorityQueueHeap.poll()+priorityQueueHeap.poll()
     }
 }
 
