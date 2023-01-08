6861e8ba41b0eec8e0179fb035a0aa0316dfcfe5
kostya05983 <kostya05983@mail.ru>
2023-01-08 17:10:21 +0700

    Day3 first

---
+++ src/main/kotlin/aoe2022/Day3.kt
@@ -0,0 +1,42 @@
+package aoe2022
+
+import java.io.File
+
+class Day3 {
+    fun solve(): Int {
+        val input =
+            File("/home/kostya05983/Projects/github/Competitions/src/main/resources/aoe/input_day_3.txt").readLines()
+
+        val total = mutableListOf<Pair<Int, Int>>()
+
+        for (j in input.indices) {
+            val line = input[j]
+            val half = line.length / 2
+            val set = hashSetOf<Char>()
+            val alreadyAdded = hashSetOf<Char>()
+            for (i in line.indices) {
+                val item = line[i]
+                if (i < half) {
+                    set.add(item)
+                } else if (set.contains(item) && !alreadyAdded.contains(item)) {
+                    alreadyAdded.add(item)
+
+                    val priority = if (item.isUpperCase()) {
+                        item.toInt() - 38
+                    } else {
+                        item.toInt() - 96
+                    }
+                    total.add(
+                        Pair(j, priority)
+                    )
+                }
+            }
+        }
+
+        return total.sumBy { it.second }
+    }
+}
+
+fun main(args: Array<String>) {
+    println(Day3().solve())
+}
