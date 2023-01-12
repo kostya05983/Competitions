e1f73165e97c1de9d22f3b8622616eb7a7544082
kostya05983 <kostya05983@mail.ru>
2023-01-13 00:22:10 +0700

    Complete day4

--- src/main/kotlin/aoe2022/Day4.kt
+++ src/main/kotlin/aoe2022/Day4.kt
@@ -4,7 +4,7 @@ import java.io.File
 
 class Day4 {
     data class Range(val start: Int, val end: Int) {
-        fun intersect(range: Range): Boolean {
+        fun include(range: Range): Boolean {
             //----0123--
             //------2345--
             if (range.start >= start && range.end <= end) {
@@ -17,9 +17,43 @@ class Day4 {
 
             return false
         }
+
+        fun intersect(range: Range): Boolean {
+            if (range.start in start..end) {
+                return true
+            }
+            if (range.end in start..end) {
+                return true
+            }
+            if (start in range.start..range.end) {
+                return true
+            }
+            if (end in range.start..range.end) {
+                return true
+            }
+
+            return false
+        }
     }
 
     fun solve(): Int {
+        val input =
+            File("/home/kostya05983/Projects/github/Competitions/src/main/resources/aoe/input_day_4.txt").readLines()
+        var counter = 0
+        for (line in input) {
+            val splitted = line.split(",")
+            val leftRange = getRange(splitted[0])
+            val rightRange = getRange(splitted[1])
+
+            if (leftRange.include(rightRange)) {
+                counter++
+            }
+        }
+
+        return counter
+    }
+
+    fun solve2(): Int {
         val input =
             File("/home/kostya05983/Projects/github/Competitions/src/main/resources/aoe/input_day_4.txt").readLines()
         var counter = 0
@@ -43,5 +77,5 @@ class Day4 {
 }
 
 fun main(args: Array<String>) {
-    println(Day4().solve())
+    println(Day4().solve2())
 }
