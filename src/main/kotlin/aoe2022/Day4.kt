6ec4182fa39de53b0d4a34f23aa03315768099e3
kostya05983 <kostya05983@mail.ru>
2023-01-13 00:19:24 +0700

    Day4 first pair

---
+++ src/main/kotlin/aoe2022/Day4.kt
@@ -0,0 +1,47 @@
+package aoe2022
+
+import java.io.File
+
+class Day4 {
+    data class Range(val start: Int, val end: Int) {
+        fun intersect(range: Range): Boolean {
+            //----0123--
+            //------2345--
+            if (range.start >= start && range.end <= end) {
+                return true
+            }
+
+            if (start >= range.start && end <= range.end) {
+                return true
+            }
+
+            return false
+        }
+    }
+
+    fun solve(): Int {
+        val input =
+            File("/home/kostya05983/Projects/github/Competitions/src/main/resources/aoe/input_day_4.txt").readLines()
+        var counter = 0
+        for (line in input) {
+            val splitted = line.split(",")
+            val leftRange = getRange(splitted[0])
+            val rightRange = getRange(splitted[1])
+
+            if (leftRange.intersect(rightRange)) {
+                counter++
+            }
+        }
+
+        return counter
+    }
+
+    private fun getRange(str: String): Range {
+        val splitted = str.split("-")
+        return Range(splitted[0].toInt(), splitted[1].toInt())
+    }
+}
+
+fun main(args: Array<String>) {
+    println(Day4().solve())
+}
