1e88abc97af12a9550a2d0bd7b7ab00ed3f36dca
kostya05983 <kostya05983@mail.ru>
2023-05-18 22:43:22 +0700

    The best solution

--- src/main/kotlin/leetCode/medium/IncrementSubmatricesByOne.kt
+++ src/main/kotlin/leetCode/medium/IncrementSubmatricesByOne.kt
@@ -5,13 +5,43 @@ class IncrementSubmatricesByOne {
         val matrix = Array(n) { IntArray(n) }
 
         for (query in queries) {
-            for (i in query[0]..query[2]) {
-                for (j in query[1]..query[3]) {
-                    matrix[i][j]++
-                }
+            val (topLeftRow, topLeftColumn) = query
+            val bottomRightRow = query[2] + 1
+            val bottomRightColumn = query[3] + 1
+
+            matrix[topLeftRow][topLeftColumn]++
+
+            if (bottomRightColumn < n) {
+                matrix[topLeftRow][bottomRightColumn]--
+            }
+
+            if (bottomRightRow < n) {
+                matrix[bottomRightRow][topLeftColumn]--
+            }
+
+            if (bottomRightRow < n && bottomRightColumn < n) {
+                matrix[bottomRightRow][bottomRightColumn]++
+            }
+        }
+
+        for (i in 0 until n) {
+            for (j in 1 until n) {
+                matrix[i][j] += matrix[i][j - 1]
+            }
+        }
+
+        for (i in 1 until n) {
+            for (j in 0 until n) {
+                matrix[i][j] += matrix[i - 1][j]
             }
         }
 
         return matrix
     }
+}
+
+fun main(args: Array<String>) {
+    val solution = IncrementSubmatricesByOne()
+    val example = arrayOf(intArrayOf(1, 1, 2, 2), intArrayOf(0, 0, 1, 1))
+    solution.rangeAddQueries(3, example)
 }
