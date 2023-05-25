b424f97d2fa08fa1c4eaaac9da91f98d9046e509
kostya05983 <kostya05983@mail.ru>
2023-05-25 22:36:33 +0700

    The best solution

--- src/main/kotlin/leetCode/medium/FriendsOfAppropriateAges.kt
+++ src/main/kotlin/leetCode/medium/FriendsOfAppropriateAges.kt
@@ -2,56 +2,102 @@ package leetCode.medium
 
 class FriendsOfAppropriateAges {
     fun numFriendRequests(ages: IntArray): Int {
-        var count = 0
-        for (i in 0 until ages.size) {
-            val origin = ages[i]
+        val ageCounts = IntArray(121)
+        for (age in ages) {
+            ageCounts[age]++
+        }
+        var leftBorder = 15
+        var result = 0
+        var window = 0
 
-            for (j in 0 until ages.size) {
-                if (i == j) continue
-                val target = ages[j]
-
-                if (isMatch(target, origin)) {
-                    count++
-                }
-                if (isMatch(target, origin)) {
-                    count++
-                }
+        for (age in 15..120) {
+            if (age.rem(2) == 0) {
+                window -= ageCounts[leftBorder]
+                leftBorder++
             }
-
+            val ageCount = ageCounts[age]
+            if (ageCount == 0) {
+                continue
+            }
+            window += ageCount
+            result += ageCount * (window - 1)
         }
 
-        return count
-    }
-
-    fun isMatch(target: Int, origin: Int): Boolean {
-        if (target <= origin.shr(1) + 7) {
-            return false
-        }
-        if (target > origin) return false
-        if (target > 100 && origin < 100) return false
-        return true
+        return result
     }
 }
 
 fun main(args: Array<String>) {
     val solution = FriendsOfAppropriateAges()
 
-//    val example = solution.numFriendRequests(intArrayOf(16, 16))
-//    require(example == 2)
-//
-//    val example1 = solution.numFriendRequests(intArrayOf(16, 17, 18))
-//    require(example1 == 2)
-//
-//    val example2 = solution.numFriendRequests(intArrayOf(20, 30, 100, 110, 120))
-//    require(example2 == 3)
+    val example = solution.numFriendRequests(intArrayOf(16, 16))
+    require(example == 2)
 
-    for (i in 1..120) {
-        for (j in i downTo 1) {
-            if (!solution.isMatch(i, j)) {
-                println("Диапазон ${i}..$j")
-                break
-            }
-        }
-    }
+    val example1 = solution.numFriendRequests(intArrayOf(16, 17, 18))
+    require(example1 == 2)
 
+    val example2 = solution.numFriendRequests(intArrayOf(20, 30, 100, 110, 120))
+    require(example2 == 3)
+
+    val example3 = solution.numFriendRequests(intArrayOf(108, 115, 5, 24, 82))
+    require(example3 == 3)
+
+    val example4 = solution.numFriendRequests(intArrayOf(118, 14, 7, 63, 103))
+    require(example4 == 2)
+
+    val example5 = solution.numFriendRequests(
+        intArrayOf(
+            81,
+            106,
+            11,
+            66,
+            83,
+            113,
+            51,
+            62,
+            47,
+            42,
+            85,
+            94,
+            78,
+            96,
+            51,
+            14,
+            3,
+            111,
+            57,
+            66,
+            8,
+            113,
+            27,
+            61,
+            21,
+            55,
+            87,
+            15,
+            20,
+            23,
+            14,
+            105,
+            38,
+            85,
+            2,
+            108,
+            103,
+            46,
+            44,
+            27,
+            79,
+            108,
+            106,
+            86,
+            113,
+            24,
+            39,
+            8,
+            7,
+            97
+        )
+    )
+    require(example5 == 434)
 }
