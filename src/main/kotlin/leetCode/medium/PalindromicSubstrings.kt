ddbedf9de442ed5753eb88a0549cf03589344eb6
kostya05983 <kostya05983@mail.ru>
2023-08-02 01:53:52 +0700

    Palindromic substrings

--- src/main/kotlin/leetCode/medium/PalindromicSubstrings.kt
+++ src/main/kotlin/leetCode/medium/PalindromicSubstrings.kt
@@ -4,7 +4,28 @@ import org.junit.jupiter.api.Assertions.assertEquals
 
 class PalindromicSubstrings {
     fun countSubstrings(s: String): Int {
-        TODO()
+        var count = 0
+
+        for (i in s.indices) {
+            var left = i
+            var right = i
+
+            while (left >= 0 && right < s.length && s[left] == s[right]) {
+                left--
+                right++
+                count++
+            }
+
+            left = i
+            right = i + 1
+            while (left >= 0 && right < s.length && s[left] == s[right]) {
+                left--
+                right++
+                count++
+            }
+        }
+
+        return count
     }
 }
 
