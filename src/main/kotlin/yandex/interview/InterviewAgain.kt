9be2768f578684321e4708cecc73f44d256653bc
kostya05983 <kostya05983@mail.ru>
2023-08-03 01:55:18 +0700

    Задачки

---
+++ src/main/kotlin/yandex/interview/InterviewAgain.kt
@@ -0,0 +1,66 @@
+package yandex.interview
+
+import java.util.Stack
+import kotlin.random.Random
+
+
+fun revert(str: String): String {
+    val sb = StringBuilder()
+
+    val stack = Stack<Char>()
+    for (ch in str) {
+        val diff = ch - 'A'
+        if ((diff < 0 || diff > 26) && ch != ' ') throw IllegalArgumentException("Неверный входные данные")
+
+        if (ch == ' ') {
+            while (!stack.isEmpty()) {
+                sb.append(stack.pop())
+            }
+            sb.append(ch)
+        } else {
+            stack.add(ch)
+        }
+    }
+    while (!stack.isEmpty()) {
+        sb.append(stack.pop())
+    }
+
+    return sb.toString()
+}
+
+fun main(args: Array<String>) {
+
+}
+
+class Randomizer {
+    private val valueToIndex = mutableMapOf<Int, Int>()
+    private val indexToValue = mutableMapOf<Int, Int>()
+    private var size = 0
+
+    fun add(value: Int) {
+        if (valueToIndex.containsKey(value)) return
+
+        valueToIndex.put(value, size)
+        indexToValue.put(size, value)
+        size++
+    }
+
+    fun contains(value: Int): Boolean {
+        return valueToIndex.containsKey(value)
+    }
+
+    fun getRandom(): Int {
+        require(size != 0) { "В рандомайзере нет чисел" }
+
+        val randomIndex = Random.nextInt(0, size - 1)
+        return indexToValue[randomIndex]!!
+    }
+
+    fun remove(value: Int) {
+        val index = requireNotNull(valueToIndex[value]) { "Значение не существует" }
+
+        indexToValue.remove(index)
+        valueToIndex.remove(value)
+        size--
+    }
+}
