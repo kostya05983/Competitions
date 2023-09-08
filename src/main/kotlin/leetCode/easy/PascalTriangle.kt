a6ba76b671460d5db1aa9c1c5f4f2c28503b7e6f
kostya05983 <kostya05983@mail.ru>
2023-09-09 00:27:38 +0700

    Better pascal triangle

--- src/main/kotlin/leetCode/easy/PascalTriangle.kt
+++ src/main/kotlin/leetCode/easy/PascalTriangle.kt
@@ -4,33 +4,25 @@ class PascalTriangle {
     fun generate(numRows: Int): List<List<Int>> {
         val result = mutableListOf<List<Int>>()
 
-        for(i in 0 until numRows) {
-            if (i == 0) {
-                result.add(generateRow(emptyList(), i))
-            } else {
-                result.add(generateRow(result[i-1], i))
-            }
+        result.add(generateRow(emptyList(), 0))
+        for (i in 1 until numRows) {
+            result.add(generateRow(result[i - 1], i))
         }
 
         return result
     }
 
     private fun generateRow(previous: List<Int>, row: Int): List<Int> {
-        if (row == 0) return listOf(1)
-        if (row == 1) return listOf(1, 1)
-
-
         val result = mutableListOf<Int>()
 
-        for (i in 0..row - 1) {
-            if (i == 0) {
+        for (i in 0..row) {
+            if (i == 0 || i == row) {
                 result.add(1)
                 continue
             }
-            val sum = previous[i-1] + previous[i]
+            val sum = previous[i - 1] + previous[i]
             result.add(sum)
         }
-        result.add(1)
 
         return result
     }
