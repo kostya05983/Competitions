8344b8c4e28773ba359f188ae2b6cca2642db652
kostya05983 <kostya05983@mail.ru>
2023-09-22 01:24:45 +0700

    Unique number of occurances

--- src/main/kotlin/leetCode/easy/UniqueNumberOfOccurrances.kt
+++ src/main/kotlin/leetCode/easy/UniqueNumberOfOccurrances.kt
@@ -1,20 +1,22 @@
 package leetCode.easy
 
 import org.junit.jupiter.api.Assertions.*
-import java.util.TreeMap
 
 class UniqueNumberOfOccurrances {
     fun uniqueOccurrences(arr: IntArray): Boolean {
-        val frequencyMap = TreeMap<Int, Int>()
+        val frequencyMap = HashMap<Int, Int>()
 
         for (i in arr.indices) {
-            frequencyMap[arr[i]] = frequencyMap.getOrDefault(arr[i], 0) + 1
+            val oldFrequency = frequencyMap.getOrDefault(arr[i], 0)
+            val newFrequency = oldFrequency + 1
+
+            frequencyMap[arr[i]] = newFrequency
         }
 
-        var previous: Map.Entry<Int, Int>? = null
+        val uniqueSet = hashSetOf<Int>()
         for (entry in frequencyMap) {
-            if (previous?.value == entry.value) return false
-            previous = entry
+            if (uniqueSet.contains(entry.value)) return false
+            uniqueSet.add(entry.value)
         }
 
         return true
@@ -31,4 +33,41 @@ fun main(args: Array<String>) {
 
     val example3 = solution.uniqueOccurrences(intArrayOf(-3, 0, 1, -3, 1, 1, 1, -3, 10, 0))
     assertTrue(example3)
+
+    val example4 = solution.uniqueOccurrences(
+        intArrayOf(
+            26,
+            2,
+            16,
+            16,
+            5,
+            5,
+            26,
+            2,
+            5,
+            20,
+            20,
+            5,
+            2,
+            20,
+            2,
+            2,
+            20,
+            2,
+            16,
+            20,
+            16,
+            17,
+            16,
+            2,
+            16,
+            20,
+            26,
+            16
+        )
+    )
+    assertFalse(example4)
+
+    val example5 = solution.uniqueOccurrences(intArrayOf(-1, -1, -1, -3, -1, -1))
+    assertTrue(example5)
 }
