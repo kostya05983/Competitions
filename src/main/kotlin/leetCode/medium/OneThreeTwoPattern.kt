61f100d9ffe467bb7bfcbf48d1d1b8483a95f95d
kostya05983 <kostya05983@mail.ru>
2023-09-30 23:59:07 +0700

    OneThreeTwoPattern

--- src/main/kotlin/leetCode/medium/OneThreeTwoPattern.kt
+++ src/main/kotlin/leetCode/medium/OneThreeTwoPattern.kt
@@ -1,43 +1,27 @@
 package leetCode.medium
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import java.util.LinkedList
 
 class OneThreeTwoPattern {
     fun find132pattern(nums: IntArray): Boolean {
         if (nums.size < 3) return false
 
-        var i = 0
-        var leftMin = nums[i]
-        var k = 1
+        val monotonicStack = LinkedList<Int>()
 
-        var middle: Int? = null
-        while (k < nums.size) {
-            if (leftMin < nums[k]) {
-                middle = nums[k]
-                break
-            } else {
-                leftMin = minOf(leftMin, nums[k])
-                i = k
-            }
-            k++
-        }
-        //массив отсортирован по убыванию
-        if (middle == null) return false
+        var middle = Int.MIN_VALUE
 
-        var j: Int = k + 1
-        while (j < nums.size) {
-            if (middle!! > nums[j] && nums[j] > nums[i]) {
-                break
-            } else {
-                if (nums[j] > middle) {
-                    middle = maxOf(middle, nums[j])
-                    k = j
-                }
+        for (i in nums.size - 1 downTo 0) {
+            val num = nums[i]
+            if (num < middle) return true
+
+            while (monotonicStack.isNotEmpty() && monotonicStack.peek() < num) {
+                middle = monotonicStack.pop()
             }
-            j++
+            monotonicStack.push(num)
         }
 
-        return j < nums.size && nums[i] < nums[k] && nums[k] > nums[j] && nums[i] < nums[j]
+        return false
     }
 }
 
@@ -55,6 +39,9 @@ fun main(args: Array<String>) {
     val example4 = solution.find132pattern(intArrayOf(1, 0, 1, -4, -3))
     assertEquals(false, example4)
 
-    val example5 = solution.find132pattern(intArrayOf(1,4,0,-1,-2,-3,-1,-2))
+    val example5 = solution.find132pattern(intArrayOf(1, 4, 0, -1, -2, -3, -1, -2))
     assertEquals(true, example5)
+
+    val example6 = solution.find132pattern(intArrayOf(3, 5, 0, 3, 4))
+    assertEquals(true, example6)
 }
