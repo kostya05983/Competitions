2f2dca344acfa73e92bd39e8204e78117cdd870b
kostya05983 <kostya05983@mail.ru>
2023-10-04 01:09:00 +0700

    Minimumbitflip

--- src/main/kotlin/leetCode/easy/MinimumBitFlipsConvertNumber.kt
+++ src/main/kotlin/leetCode/easy/MinimumBitFlipsConvertNumber.kt
@@ -1,4 +1,20 @@
 package leetCode.easy
 
 class MinimumBitFlipsConvertNumber {
+    fun minBitFlips(start: Int, goal: Int): Int {
+        var xor = start xor goal
+        var count = 0
+
+        while (xor > 0) {
+            count += xor and 1
+            xor = xor shr 1
+        }
+
+        return count
+    }
+}
+
+fun main(args: Array<String>) {
+    val solution = MinimumBitFlipsConvertNumber()
+    solution.minBitFlips(1, 4)
 }
