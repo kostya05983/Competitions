acf9c067a4f71b4970da9200eb0cbbecc376772f
kostya05983 <kostya05983@mail.ru>
2023-10-19 03:57:51 +0700

    Subset with dups

--- src/main/kotlin/leetCode/medium/SubsetWithDups.kt
+++ src/main/kotlin/leetCode/medium/SubsetWithDups.kt
@@ -1,4 +1,26 @@
 package leetCode.medium
 
 class SubsetWithDups {
+    fun subsetsWithDup(nums: IntArray): List<List<Int>> {
+        nums.sort()
+        val result = mutableListOf<List<Int>>()
+
+        subsets(0, mutableListOf(), nums, result)
+
+        return result.toList()
+    }
+
+    private fun subsets(i: Int, current: MutableList<Int>, nums: IntArray, result: MutableList<List<Int>>) {
+        result.add(current.toList())
+
+        for (j in i until nums.size) {
+            if (j == i || nums[j] != nums[j - 1]) {
+                current.add(nums[j])
+
+                subsets(j + 1, current, nums, result)
+
+                current.removeAt(current.size - 1)
+            }
+        }
+    }
 }
