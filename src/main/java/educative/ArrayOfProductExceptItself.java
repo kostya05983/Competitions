d58a247c0fea2454c5133d8550042cce57addd19
kostya05983 <kostya05983@mail.ru>
2023-10-31 02:29:42 +0700

    Array of product except itself

---
+++ src/main/java/educative/ArrayOfProductExceptItself.java
@@ -0,0 +1,31 @@
+package educative;
+
+public class ArrayOfProductExceptItself {
+    public static int[] findProduct(int arr[]) {
+        int product = 1;
+
+        int zeroCount = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                zeroCount++;
+                continue;
+            }
+
+            product *= arr[i];
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0 && zeroCount == 1) {
+                arr[i] = product;
+                continue;
+            }
+            if (zeroCount > 0) {
+                arr[i] = 0;
+                continue;
+            }
+            arr[i] = product / arr[i];
+        }
+
+        return arr;
+    }
+}
