23a131b1798570218e26c1db7b05874c8c41bdf7
kostya05983 <kostya05983@mail.ru>
2023-10-31 02:53:55 +0700

    RearangePositiveNegativeNumbers

---
+++ src/main/java/educative/RearangePositiveNegativeNumbers.java
@@ -0,0 +1,18 @@
+package educative;
+
+public class RearangePositiveNegativeNumbers {
+    public static void reArrange(int[] arr) {
+        int left = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0 && left < i) {
+                while (arr[left] < 0) {
+                    left++;
+                }
+                int temp = arr[left];
+                arr[left] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+}
