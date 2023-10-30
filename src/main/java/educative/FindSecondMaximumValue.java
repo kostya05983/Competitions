935de3ffc85317eae04083fad6aa64a84c22c9fa
kostya05983 <kostya05983@mail.ru>
2023-10-31 02:47:09 +0700

    Find second maximum value

---
+++ src/main/java/educative/FindSecondMaximumValue.java
@@ -0,0 +1,21 @@
+package educative;
+
+public class FindSecondMaximumValue {
+    public int findSecondMaximum(int[] arr) {
+        int firstMax = arr[0];
+        int secondMax = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > firstMax) {
+                secondMax = firstMax;
+                firstMax = arr[i];
+            }
+            if (arr[i] != firstMax && arr[i] > secondMax) {
+                secondMax = arr[i];
+            }
+        }
+
+        // Write - Your - Code
+        return secondMax;
+    }
+}
