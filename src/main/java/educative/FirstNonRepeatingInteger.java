8f8f2713cfd55ca06ddcf8d613b9c17bef18bbb1
kostya05983 <kostya05983@mail.ru>
2023-10-31 02:40:51 +0700

    First non repeating integer

---
+++ src/main/java/educative/FirstNonRepeatingInteger.java
@@ -0,0 +1,18 @@
+package educative;
+
+public class FirstNonRepeatingInteger {
+    public static int findFirstUnique(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (i != j && arr[i] == arr[j]) {
+                    break;
+                }
+                if (j == arr.length-1) {
+                    return arr[i];
+                }
+            }
+        }
+
+        return -1;
+    }
+}
