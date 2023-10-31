d39ef40524479906171e25716d29f6df3db42e4d
kostya05983 <kostya05983@mail.ru>
2023-11-01 03:13:45 +0700

    MiddleNodeLinkedList

---
+++ src/main/java/educative/crack/java/interview/linkedlist/MiddleNodeLinkedList.java
@@ -0,0 +1,32 @@
+package educative.crack.java.interview.linkedlist;
+
+public class MiddleNodeLinkedList {
+
+    public static <T> Object findMiddle(SinglyLinkedList<T> list) {
+        SinglyLinkedList.Node current = list.headNode;
+
+        int count = 0;
+        while (current != null) {
+            count++;
+            current = current.nextNode;
+        }
+
+        int midIndex = count / 2;
+        if (count % 2 == 0) {
+            midIndex--;
+        }
+
+        int index = 0;
+        current = list.headNode;
+        while (current != null) {
+            if (midIndex == index) {
+                return current.data;
+            }
+            index++;
+            current = current.nextNode;
+        }
+
+        // Write -- Your -- Code
+        return -1;
+    }
+}
