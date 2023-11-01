57431bc6dfc962dd0af764939c18e40816947eee
kostya05983 <kostya05983@mail.ru>
2023-11-02 00:57:29 +0700

    GetMaximumInGeneratedArray

---
+++ src/main/kotlin/leetCode/easy/GetMaximumInGeneratedArray.kt
@@ -0,0 +1,35 @@
+package leetCode.easy
+
+class GetMaximumInGeneratedArray {
+    fun getMaximumGenerated(n: Int): Int {
+        val dp = IntArray(101)
+        dp[0] = 0
+        if (n == 0) return 0
+
+        dp[1] = 1
+        var currentN = 1
+
+        var max = 1
+
+        while (currentN < n) {
+            currentN++
+
+            if (currentN.rem(2) == 0) {
+                dp[currentN] = dp[currentN / 2]
+            } else {
+                dp[currentN] = dp[currentN / 2] + dp[currentN / 2 + 1]
+            }
+            max = maxOf(dp[currentN], max)
+        }
+
+        return max
+    }
+}
+
+fun main(args: Array<String>) {
+    val solution = GetMaximumInGeneratedArray()
+//    println(solution.getMaximumGenerated(7))
+//    println(solution.getMaximumGenerated(2))
+//    println(solution.getMaximumGenerated(3))
+    println(solution.getMaximumGenerated(100))
+}
