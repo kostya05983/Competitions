126b956731d9fb23a837bf68a52fe359bfd0b17a
kostya05983 <kostya05983@mail.ru>
2023-11-09 03:57:39 +0700

    MaxNumberKSumPairs

--- src/main/kotlin/leetCode/medium/MaxNumberKSumPairs.kt
+++ src/main/kotlin/leetCode/medium/MaxNumberKSumPairs.kt
@@ -2,6 +2,39 @@ package leetCode.medium
 
 class MaxNumberKSumPairs {
     fun maxOperations(nums: IntArray, k: Int): Int {
-        TODO()
+        val hashMap = HashMap<Int, Int>()
+        for (num in nums) {
+            hashMap[num] = hashMap.getOrDefault(num, 0) + 1
+        }
+
+        var result = 0
+        var i = 0
+        while (i < nums.size) {
+            if (hashMap[nums[i]] == 0) {
+                i++
+                continue
+            }
+
+            val other = k - nums[i]
+
+            hashMap[nums[i]] = hashMap[nums[i]]!! - 1
+
+            val otherFrequency = hashMap[other]
+            if (otherFrequency == null || otherFrequency == 0) {
+                i++
+                continue
+            }
+            hashMap[other] = otherFrequency - 1
+            result++
+            i++
+        }
+
+        return result
     }
+}
+
+fun main(args: Array<String>) {
+    val solution = MaxNumberKSumPairs()
+    println(solution.maxOperations(intArrayOf(1, 2, 3, 4), 5))
+    println(solution.maxOperations(intArrayOf(3, 1, 3, 4, 3), 6))
 }
