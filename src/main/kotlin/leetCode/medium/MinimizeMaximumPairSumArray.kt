f8202fe3901812b62cbf5c2eb4a25675c2cc1493
kostya05983 <kostya05983@mail.ru>
2023-11-17 21:15:26 +0700

    MinimizeMaximumPairSUmArray

---
+++ src/main/kotlin/leetCode/medium/MinimizeMaximumPairSumArray.kt
@@ -0,0 +1,26 @@
+package leetCode.medium
+
+class MinimizeMaximumPairSumArray {
+    fun minPairSum(nums: IntArray): Int {
+        nums.sort()
+
+        var left = 0
+        var right = nums.size - 1
+
+        var max = Int.MIN_VALUE
+        while (left < right) {
+            max = maxOf(nums[left] + nums[right], max)
+            right--
+            left++
+        }
+
+        return max
+    }
+}
+
+fun main(args: Array<String>) {
+    val solution = MinimizeMaximumPairSumArray()
+//    println(solution.minPairSum(intArrayOf(3, 5, 2, 3)))
+//    println(solution.minPairSum(intArrayOf(3, 5, 4, 2, 4, 6)))
+    println(solution.minPairSum(intArrayOf(4, 1, 5, 1, 2, 5, 1, 5, 5, 4)))
+}
