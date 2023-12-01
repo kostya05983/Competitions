89ba635c41737f1408b06458403b84bb0730ea31
kostya05983 <kostya05983@mail.ru>
2023-12-02 01:19:51 +0700

    CheckIfTwoStringArraysAreEquivalent

---
+++ src/main/kotlin/leetCode/easy/CheckIfTwoStringArraysAreEquivalent.kt
@@ -0,0 +1,7 @@
+package leetCode.easy
+
+class CheckIfTwoStringArraysAreEquivalent {
+    fun arrayStringsAreEqual(word1: Array<String>, word2: Array<String>): Boolean {
+        return word1.joinToString("") == word2.joinToString("")
+    }
+}
