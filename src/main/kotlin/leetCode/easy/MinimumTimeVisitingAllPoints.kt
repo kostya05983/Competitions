587fde5bf82d3cadd94d9b6f56de9e80e08d9377
kostya05983 <kostya05983@mail.ru>
2023-12-04 02:14:13 +0700

    MinimumTimeVisitingAllPoints

--- src/main/kotlin/leetCode/easy/MinimumTimeVisitingAllPoints.kt
+++ src/main/kotlin/leetCode/easy/MinimumTimeVisitingAllPoints.kt
@@ -7,12 +7,55 @@ class MinimumTimeVisitingAllPoints {
         var pointerX = points[0][0]
         var pointerY = points[0][1]
 
+        var result = 0
         for (i in 1 until points.size) {
             val point = points[i]
-            val diffX = pointerX - point[0]
 
+            while (point[0] != pointerX || point[1] != pointerY) {
+                val diffX = point[0] - pointerX
+                val diffY = point[1] - pointerY
+                val min = minOf(abs(diffX), abs(diffY))
+
+                when {
+                    diffX > 0 && diffY > 0 -> {
+                        //right up in diagonal
+                        pointerX += min
+                        pointerY += min
+                    }
+
+                    diffX < 0 && diffY > 0 -> {
+                        //left up in diagonal
+                        pointerX -= min
+                        pointerY += min
+                    }
+
+                    diffX > 0 && diffY < 0 -> {
+                        //right down in diagonal
+                        pointerX += min
+                        pointerY -= min
+                    }
+
+                    diffX < 0 && diffY < 0 -> {
+                        //left down in diagonal
+                        pointerX -= min
+                        pointerY -= min
+                    }
+
+                    else -> {
+                        if (diffX != 0) {
+                            pointerX = point[0]
+                            result += abs(diffX)
+                        } else {
+                            pointerY = point[1]
+                            result += abs(diffY)
+                        }
+                    }
+                }
+                result += min
+            }
         }
-        TODO()
+
+        return result
     }
 }
 
