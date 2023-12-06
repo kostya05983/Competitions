9bffde421585804f5570417765fc5c13e95e5bc0
kostya05983 <kostya05983@mail.ru>
2023-12-07 00:40:19 +0700

    Count distinct numbers on board

---
+++ src/main/kotlin/leetCode/easy/CountDistinctNumbersOnBoard.kt
@@ -0,0 +1,10 @@
+package leetCode.easy
+
+import java.util.LinkedList
+
+class CountDistinctNumbersOnBoard {
+    fun distinctIntegers(n: Int): Int {
+        if (n == 1) return 1
+        return n - 1
+    }
+}
