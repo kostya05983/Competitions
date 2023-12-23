6728cea8a1b76c85b068a7aabb747a0a311f7653
kostya05983 <kostya05983@mail.ru>
2023-12-23 14:46:08 +0700

    Stylish

--- src/main/kotlin/leetCode/medium/DivideArrayIntoArraysWithMaxDiff.kt
+++ src/main/kotlin/leetCode/medium/DivideArrayIntoArraysWithMaxDiff.kt
@@ -6,15 +6,14 @@ class DivideArrayIntoArraysWithMaxDiff {
 
         val result = Array(nums.size / 3) { IntArray(3) }
 
-        for (i in 0 until nums.size step 3) {
-
+        for (i in nums.indices step 3) {
             if (nums[i + 2] - nums[i] > k || nums[i + 2] - nums[i + 1] > k
                 || nums[i + 1] - nums[i] > k
             ) return emptyArray()
-            val index = i/3
+            val index = i / 3
             result[index][0] = nums[i]
-            result[index][1] = nums[i+1]
-            result[index][2] = nums[i+2]
+            result[index][1] = nums[i + 1]
+            result[index][2] = nums[i + 2]
         }
 
         return result
@@ -23,8 +22,8 @@ class DivideArrayIntoArraysWithMaxDiff {
 
 fun main(args: Array<String>) {
     val solution = DivideArrayIntoArraysWithMaxDiff()
-    println(solution.divideArray(intArrayOf(1,3,4,8,7,9,3,5,1),2).map {
+    println(solution.divideArray(intArrayOf(1, 3, 4, 8, 7, 9, 3, 5, 1), 2).map {
         it.toList()
     })
-    println(solution.divideArray(intArrayOf(1,3,3,2,7,3),3).map { it.toList() })
+    println(solution.divideArray(intArrayOf(1, 3, 3, 2, 7, 3), 3).map { it.toList() })
 }
