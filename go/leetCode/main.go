cfb2f613ae2e9ec249a397e1dcb2b793ee2f0320
kostya05983 <kostya05983@mail.ru>
2024-02-16 00:28:50 +0700

    LargestPerimiter

--- go/leetCode/main.go
+++ go/leetCode/main.go
@@ -1,4 +1,10 @@
 package main
 
+import (
+	"./medium"
+	"fmt"
+)
+
 func main() {
+	fmt.Println(medium.LargestPerimeter([]int{1, 12, 1, 2, 5, 50, 3}))
 }
--- go/leetCode/medium/FindPolygonWithTheLargestPerimeter.go
+++ go/leetCode/medium/FindPolygonWithTheLargestPerimeter.go
@@ -1,9 +1,33 @@
 package medium
 
-import "sort"
+import (
+	"sort"
+)
+
+func LargestPerimeter(nums []int) int64 {
+	return largestPerimeter(nums)
+}
 
 func largestPerimeter(nums []int) int64 {
 	sort.Ints(nums)
 
+	prefixSum := make([]int64, len(nums))
 
+	var sum int64 = 0
+
+	for i := 0; i < len(nums); i++ {
+		sum += int64(nums[i])
+		prefixSum[i] = sum
+	}
+
+	var result int64 = -1
+	for i := len(nums) - 1; i >= 2; i-- {
+		num := int64(nums[i])
+
+		if prefixSum[i-1] > num {
+			result = max(result, num+prefixSum[i-1])
+		}
+	}
+
+	return result
 }
