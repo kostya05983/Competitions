626a5691e7b94a30ee48b92fbb50058e194c5743
kostya05983 <kostya05983@mail.ru>
2024-04-02 00:22:19 +0700

    Counting words with given prefix

---
+++ go/leetCode/easy/CountingWordsWithGivenPrefix.go
@@ -0,0 +1,15 @@
+package easy
+
+import "strings"
+
+func prefixCount(words []string, pref string) int {
+	result := 0
+
+	for _, word := range words {
+		if strings.HasPrefix(word, pref) {
+			result++
+		}
+	}
+
+	return result
+}
