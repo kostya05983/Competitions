87069c51effdbbff0abad5d54149ee299372c8f1
kostya05983 <kostya05983@mail.ru>
2024-05-12 14:19:58 +0700

    Minimum cost to buy apples

--- go/leetCode/medium/minimum_cost_to_buy_apples/MinimumCostToBuyApples.go
+++ go/leetCode/medium/minimum_cost_to_buy_apples/MinimumCostToBuyApples.go
@@ -1,61 +1,100 @@
 package minimum_cost_to_buy_apples
 
+import (
+	"container/heap"
+	"math"
+)
+
 type Node struct {
 	to   int
 	cost int
 }
 
 func minCost(n int, roads [][]int, appleCost []int, k int) []int64 {
-
-	graph := make(map[int]Node, 0)
+	graph := make(map[int][]Node, 0)
 
 	for _, v := range roads {
-		graph[v[0]] = Node{
-			to:   v[1],
-			cost: v[2],
+		from := v[0] - 1
+		to := v[1] - 1
+
+		_, ok := graph[from]
+		if !ok {
+			graph[from] = make([]Node, 0)
 		}
-		graph[v[1]] = Node{
-			to:   v[0],
-			cost: v[2],
+
+		_, ok = graph[to]
+		if !ok {
+			graph[to] = make([]Node, 0)
 		}
+
+		graph[from] = append(graph[from], Node{
+			to:   to,
+			cost: v[2],
+		})
+
+		graph[to] = append(graph[to], Node{
+			to:   from,
+			cost: v[2],
+		})
 	}
 
-	for i := 1; i <= n; i++ {
-
+	result := make([]int64, n)
+	for i := 0; i < n; i++ {
+		result[i] = bestCityPrice(i, graph, appleCost, k, n)
 	}
 
-	return []int64{}
+	return result
 }
 
-func bestCityPrice(start int, graph map[int][]Node) int {
-	distances := make(map[int]int, 0)
-	//todo надо по дефолту заполнить бесконечностями, можно так же превратить это в массив
+type IntHeap [][]int
 
-	distances[start] = 0
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i][0] < h[j][0] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *IntHeap) Push(x any) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*h = append(*h, x.([]int))
+}
 
-	queue := make([]Node, 0)
-	queue = append(queue, Node{start, 0})
+func (h *IntHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func bestCityPrice(start int, graph map[int][]Node, appleCost []int, k int, n int) int64 {
+	travelCosts := make([]int, n)
+	for i := range travelCosts {
+		travelCosts[i] = math.MaxInt
+	}
+	travelCosts[start] = 0
+
+	queue := IntHeap{}
+
+	heap.Init(&queue)
+	heap.Push(&queue, []int{0, start})
+	var result int64 = math.MaxInt64
 
 	for len(queue) > 0 {
-		popped := queue[0]
-		queue = queue[1:]
+		current := heap.Pop(&queue).([]int)
+		travelCost := current[0]
+		currentCity := current[1]
 
-		if popped.cost > distances[popped.to] {
-			continue
-		}
+		multiply := int64(appleCost[currentCity]) + (int64(k)+1)*int64(travelCost)
+		result = min(result, multiply)
 
-		paths := graph[popped.to]
-
-		for _, v := range paths {
-			distance := popped.cost + v.cost
-
-			if distance < distances[v.to] {
-				distances[v.to] = distance
-				//heap.Push(queue, Node{distance, v.to})
-
-				//todo тут где-то считаем и находим минимальную стоимость
+		for _, v := range graph[currentCity] {
+			neighbor := v.to
+			nextCost := travelCost + v.cost
+			if nextCost < travelCosts[neighbor] {
+				travelCosts[neighbor] = nextCost
+				heap.Push(&queue, []int{nextCost, neighbor})
 			}
 		}
 	}
-	return 0
+
+	return result
 }
